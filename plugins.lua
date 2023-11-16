e5c50f814db9aabbdd81ad3a77af6c6eb099e4ef
kazu <48248903+kazudotdev@users.noreply.github.com>
2023-11-17 00:07:17 +0900

    support monorepo with denols and tsserver

--- plugins.lua
+++ plugins.lua
@@ -31,12 +31,56 @@ return {
     config = function()
       local default_configs = require "plugins.configs.lspconfig"
       local lsp_config = require "lspconfig"
+
+      local function root_pattern_exclude(opt)
+        local lspuil = lsp_config.util
+        return function (fname)
+          local excluded_root = lspuil.root_pattern(opt.exclude)(fname)
+          local included_root = lspuil.root_pattern(opt.root)(fname)
+          if excluded_root then
+            return nil
+          else
+            return included_root
+          end
+        end
+      end
       require("mason-lspconfig").setup_handlers {
         function(server_name)
-          lsp_config[server_name].setup {
+          local setup_opts = {
             on_attach = default_configs.on_attach,
             capabilities = default_configs.capabilities,
           }
+          if server_name == "denols" then
+            setup_opts = {
+              on_attach = default_configs.on_attach,
+              capabilities = default_configs.capabilities,
+              root_dir = lsp_config.util.root_pattern("deno.json"),
+              init_options = {
+                init = true,
+                unstable = true,
+                suggest = {
+                  imports = {
+                    hosts = {
+                      ["https://deno.land"] = true,
+                      ["https://cdn.nest.land"] = true,
+                      ["https://crux.land"] = true,
+                    },
+                  },
+                },
+              },
+            }
+          elseif server_name == "tsserver" then
+            setup_opts = {
+              on_attach = default_configs.on_attach,
+              capabilities = default_configs.capabilities,
+              root_dir = root_pattern_exclude({
+                root = { "package.json" },
+                exclude = { "deno.json" },
+              }),
+              single_file_support = false,
+            }
+          end
+          lsp_config[server_name].setup(setup_opts)
         end,
       }
     end,
